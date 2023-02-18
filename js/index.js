cf8a43c0951f8e0a36cbe26f15490ef814f4acb5
Eldor06 <eldorergashev20sm>
2023-02-18 17:26:39 +0500

    Add existing project files to Git

--- js/index.js
+++ js/index.js
@@ -1,85 +1,34 @@
-let slides = document.querySelectorAll('.offer__slide')
-let prev = document.querySelector('.offer__slider-prev')
-let next = document.querySelector('.offer__slider-next')
-let current = document.querySelector('#current')
-let total = document.querySelector('#total')
+const slides = document.querySelectorAll('.offer__slide')
+const prev = document.querySelector('.offer__slider-prev')
+const next = document.querySelector('.offer__slider-next')
+const current = document.querySelector('#current')
+const total = document.querySelector('#total')
+total.innerHTML = slides.length
 
 // Slider
-let index = -1
-let currentNum = 0
-next.onclick = () => {
-    slides.forEach(el => {
-        el.style.display = 'none'; el.classList.remove('fade')
-    })
-    if (index < slides.length - 1) {
-        currentNum++
-        current.innerHTML = currentNum
-        index++
-        slides[index].style.display = 'block'; slides[index].classList.add('fade')
-    } else {
-        currentNum = 1
-        current.innerHTML = currentNum
-        index = 0
-        slides[index].style.display = 'block'; slides[index].classList.add('fade')
+let slideIndex = 1
+function showSlides(n) {
+    if (n > slides.length) {
+        slideIndex = 1
     }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
+    slides.forEach(slide => slide.classList.add('hide'))
+    slides[slideIndex - 1].classList.remove('hide')
+    slides[slideIndex - 1].classList.add('fade')
+    current.innerHTML = slideIndex
+}
+showSlides(slideIndex)
+next.onclick = () => {
+    slideIndex++
+    showSlides(slideIndex)
 }
 prev.onclick = () => {
-    slides.forEach(el => {
-        el.style.display = 'none'; el.classList.remove('fade')
-    })
-    if (index < 1) {
-        currentNum = total.innerHTML
-        current.innerHTML = currentNum
-        index = slides.length - 1
-        slides[index].style.display = 'block'; slides[index].classList.add('fade')
-    } else {
-        currentNum--
-        current.innerHTML = currentNum
-        index--
-        slides[index].style.display = 'block'; slides[index].classList.add('fade')
-    }
+    slideIndex--
+    showSlides(slideIndex)
 }
 // Slider
-
-
-// Teacher's slider
-
-// let slideIndex = 1
-// function showSlides(n) {
-//     if(n > slides.length) {
-//         slideIndex = 1
-//     }
-//     if(n < 1) {
-//         slideIndex = slides.length
-//     }
-//     slides.forEach(slide => slide.classList.add('hide'))
-//     slides[slideIndex -1].classList.remove('hide')
-//     slides[slideIndex -1].classList.add('fade')
-// }
-// showSlides(slideIndex)
-// let current = document.querySelector('#current')
-// let total = document.querySelector('#total')
-// console.log(+current.innerHTML);
-// next.onclick = () => {
-//     slideIndex++
-//     if(+current.innerHTML < +total.innerHTML) {
-//         +current.innerHTML++
-//     } else {
-//         current.innerHTML = 4
-//     }
-//     showSlides(slideIndex)
-// }
-// prev.onclick = () => {
-//     if(+current.innerHTML > 1) {
-//         +current.innerHTML--
-//     } else {
-//         current.innerHTML = 1
-//     }
-//     slideIndex--
-//     showSlides(slideIndex)
-// }
-
-// Teacher's slider
 
 
 // TAB
@@ -120,4 +69,128 @@ modalClosers.forEach(closer => {
         modalContent.classList.remove('modal__content_active')
     }
 })
-//Modal
+// Modal
+
+
+// Calorie counter
+
+let gens = document.querySelectorAll('#gender .calculating__choose-item')
+let inputs = document.querySelectorAll('.calculating__choose_medium input')
+let actBtns = document.querySelectorAll('.calculating__choose_big [data-act]')
+let resultView = document.querySelector('.calculating__result span')
+
+let userData = {
+    gender: "woman",
+}
+
+gens.forEach(btn => {
+    btn.onclick = () => {
+        gens.forEach(el => el.classList.remove('calculating__choose-item_active'))
+        btn.classList.add('calculating__choose-item_active')
+
+        let g = btn.getAttribute('data-g')
+
+        userData.gender = g
+    }
+})
+
+inputs.forEach(inp => {
+    inp.onkeyup = () => {
+        let key = inp.id
+        let val = inp.value
+
+        userData[key] = val
+    }
+})
+
+actBtns.forEach(btn => {
+    btn.onclick = () => {
+        actBtns.forEach(el => el.classList.remove('calculating__choose-item_active'))
+        btn.classList.add('calculating__choose-item_active')
+
+        let activeCount = btn.getAttribute('data-act')
+
+        let { active, gender, weight, height, age } = userData
+
+        active = activeCount
+
+        if (gender === 'woman') {
+            let res = 655.1 + 9.563 * weight + 1.85 * height - 4.676 * age;
+            resultView.innerHTML = Math.round(res * active)
+        } else {
+            let res = 66.5 + 13.75 * weight + 5.003 * height - 6.775 * age
+            resultView.innerHTML = Math.round(res * active)
+        }
+    }
+})
+
+// Calorie counter
+
+
+// Timer
+
+let deadline = "2023-02-18 18:30"
+
+function remainingDate(endTime) {
+    let t = Date.parse(endTime) - Date.parse(new Date()),
+        days = Math.floor((t / 1000) / 60 / 60 / 24),
+        hours = Math.floor(((t / 1000) / 60 / 60) % 24),
+        minutes = Math.floor(((t / 1000) / 60) % 60),
+        seconds = Math.floor((t / 1000) % 60);
+
+    return {
+        t,
+        days,
+        hours,
+        minutes,
+        seconds
+    }
+}
+
+
+function setTime(endTime, selector) {
+    let t = document.querySelector(selector),
+        days = t.querySelector('#days'),
+        hours = t.querySelector('#hours'),
+        minutes = t.querySelector('#minutes'),
+        seconds = t.querySelector('#seconds'),
+        updateTime = setInterval(showTime, 1000);
+
+    function showTime() {
+        let t = remainingDate(endTime)
+        days.innerHTML = t.days
+        hours.innerHTML = t.hours
+        minutes.innerHTML = t.minutes
+        seconds.innerHTML = t.seconds
+
+        if (t.t <= 0) {
+            clearInterval(updateTime)
+        }
+    }
+}
+
+setTime(deadline, '.timer')
+
+// Timer
+
+// onscrollend
+
+let deliveryLink = document.querySelector('.header__links [data-delivery]')
+let sectionTwoLink = document.querySelector('.header__links [data-section-two]')
+let offer = document.querySelector('.offer')
+let order = document.querySelector('.order')
+
+function scrollAndShow(link, place) {
+    link.onclick = () => {
+        place.scrollIntoView({
+            block: "center",
+            inline: "nearest",
+            behavior: "smooth"
+        })
+    }
+}
+
+scrollAndShow(deliveryLink, offer)
+scrollAndShow(sectionTwoLink, order)
+
+// onscrollend
